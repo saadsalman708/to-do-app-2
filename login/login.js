57019a0f8f05d9d33de6b3d70ee79d1f669e70be
SaadSalman708 <saadsalman708@gmail.com>
2026-02-05 05:21:03 +0500

    fixed some UI

--- login/login.js
+++ login/login.js
@@ -1,6 +1,6 @@
 import {
-    auth, signInWithEmailAndPassword, signInWithPopup, googleProvider, 
-    db , getDoc, doc , setDoc, 
+    auth, signInWithEmailAndPassword, signInWithPopup, googleProvider,
+    db, getDoc, doc, setDoc,
 } from "../firebase/config.js";
 
 const email = document.querySelector("#email");
@@ -12,36 +12,50 @@ const googleBtn = document.querySelector("#googleBtn");
 
 
 
-async function handleRedirect(userId) {
+async function handleRedirect(userId , isNew) {
 
-    const snap = await getDoc(doc(db , "users" , userId));
+    const snap = await getDoc(doc(db, "users", userId));
 
-    if (!snap.exists()) return ;
-    
+    if (!snap.exists()) return;
+
     const profile = snap.data();
-
+    let titleMsg = "Login Successful";
+    let textMsg = "Welcome User";
+    let iconMsg = "success";
+    let pageRedirect = "../user/user.html";
+    
     if (!profile.isVerified) {
-        swal.fire({
-            title: "Not Verified!",
-            text: "Admin has not approved yet!",
-            icon: "warning",
-            backdrop: `
+        titleMsg = "Not Verified!";
+        textMsg = "Admin has not approved yet!";
+        iconMsg = "warning";
+        pageRedirect = "../getIn.html";
+    };
+
+    if (profile.role === "admin") {
+        titleMsg = "Login Successful";
+        textMsg = "Welcome Admin";
+        iconMsg = "success";
+        pageRedirect = "../admin/admin.html";
+    }
+
+    if (isNew) {
+        titleMsg = "Account Created!"
+        textMsg = "Admin has not approved yet! Wait for admin aproval";
+    }
+
+    swal.fire({
+        title: titleMsg,
+        text: textMsg,
+        icon: iconMsg,
+        backdrop: `
             rgba(0,0,123,0.4)
             url("https://images.steamusercontent.com/ugc/974353111661482849/4706D02264975280AEFACD65BF02F585F978B6B3/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false")
             left top / 30%
             no-repeat`,
-        }).then(()=> {
-            window.location.href = "../getIn.html";
-        });
-        return;
-    };
-    
-    if (profile.role === "admin") {
-        window.location.href = "../admin/admin.html";
-    } else {
-        window.location.href = "../user/user.html";
-    };
-    
+    }).then(() => {
+        window.location.href = pageRedirect;
+    });
+
 };
 
 
@@ -69,7 +83,6 @@ btn.addEventListener("click", async () => {
     try {
 
         const res = await signInWithEmailAndPassword(auth, emailVal, passwordVal);
-
         await handleRedirect(res.user.uid);
 
     } catch (error) {
@@ -95,20 +108,23 @@ googleBtn.addEventListener("click", async () => {
     try {
 
         const res = await signInWithPopup(auth, googleProvider);
-        const snap = await getDoc(doc(db , "users" , res.user.uid));
+        const snap = await getDoc(doc(db, "users", res.user.uid));
+        let isNew = false;
 
         if (!snap.exists()) {
-            
-            await setDoc(doc(db , "users" , res.user.uid) , {
-                email: res.user.email,  
+
+            await setDoc(doc(db, "users", res.user.uid), {
+                email: res.user.email,
                 fullName: res.user.displayName || "",
                 role: "user",
                 isVerified: false,
                 createdAt: Date.now(),
-            } , {merge: true});
+            }, { merge: true });
+
+            isNew = true;
         }
 
-        await handleRedirect(res.user.uid);
+        await handleRedirect(res.user.uid , isNew);
 
     } catch (error) {
         Swal.fire({
--- signup/signup.js
+++ signup/signup.js
@@ -141,7 +141,7 @@ async function handleRedirect(uid , isNew) {
             url("https://images.steamusercontent.com/ugc/974353111661482849/4706D02264975280AEFACD65BF02F585F978B6B3/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false")
             left top / 30%
             no-repeat`,
-    }).then(async () => {
+    }).then(() => {
         window.location.href = pageRedirect;
     });
 }
